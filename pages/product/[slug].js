72a88baa07366610d473363024de5e2ec015d6f4
Luigi <lordwells79@hotmail.it>
2022-09-20 11:31:27 +0200

    bug fixed

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,8 +13,7 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, setQty, resetQty, onAdd, setShowCart } =
-    useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
   const handleBuyNow = () => {
     onAdd(product, qty);
